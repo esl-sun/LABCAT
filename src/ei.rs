c631617671d7ced9ef9a347c23558265a2fe9792
Emile Visser <emile.visser@gmail.com>
2024-01-17 15:44:48 +0200

    Added initial EI impl

--- src/ei.rs
+++ src/ei.rs
@@ -1,32 +1,96 @@
 use std::marker::PhantomData;
 
-use crate::{dtype, BayesianSurrogate, Surrogate, SurrogateMemory, SurrogateMaxMin};
+use lazy_static::lazy_static;
+use ord_subset::OrdSubset;
+use statrs::distribution::{Continuous, ContinuousCDF, Normal};
+// use rand_distr::{Normal, Distribution};
 
-pub trait AcqFunction<T>
+use crate::{
+    dtype,
+    memory::{ObservationIO, ObservationMaxMin},
+    BayesianSurrogate, Memory, Surrogate,
+};
+
+pub trait AcqFunction<T, M, S>
 where
     T: dtype,
+    M: ObservationIO<T>,
+    S: Surrogate<T, M>,
 {
-    fn probe_acq(&self, x: &[T]) -> T;
+    fn probe_acq(&self, surrogate: &S, x: &[T]) -> Option<T>;
 }
 
-pub struct EI<'a, T, S>
+pub trait AcqJacobian<T, M, S>
 where
     T: dtype,
-    S: Surrogate<T> + SurrogateMemory<T> + BayesianSurrogate<T>,
+    M: ObservationIO<T>,
+    S: Surrogate<T, M>,
+{
+    fn acq_jacobian(&self, surrogate: &S, x: &[T]) -> &[T];
+}
+
+lazy_static! {
+    static ref NORMAL: Normal = Normal::new(0.0, 1.0).unwrap();
+}
+
+pub struct EI<T>
+where
+    T: dtype + OrdSubset,
 {
     data_type: PhantomData<T>,
-    surrogate: &'a S,
+    xi: T,
 }
 
-impl<T, S> AcqFunction<T> for EI<'_, T, S>
+impl<T> Default for EI<T>
 where
-    T: dtype,
-    S: Surrogate<T> + SurrogateMemory<T> + SurrogateMaxMin<T> + BayesianSurrogate<T>,
+    T: dtype + OrdSubset,
 {
-    fn probe_acq(&self, x: &[T]) -> T {
-        let mean = self.surrogate.probe(x);
-        let var = self.surrogate.probe_variance(x);
-        let z = self.surrogate.memory();
-        todo!()
+    fn default() -> Self {
+        Self {
+            data_type: Default::default(),
+            xi: T::zero(),
+        }
     }
 }
+
+impl<T> EI<T>
+where
+    T: dtype + OrdSubset,
+{
+    fn new(xi: T) -> Self {
+        Self {
+            data_type: PhantomData,
+            xi,
+        }
+    }
+}
+
+impl<T, M, S> AcqFunction<T, M, S> for EI<T>
+where
+    T: dtype + OrdSubset,
+    M: ObservationIO<T> + ObservationMaxMin<T>,
+    S: Surrogate<T, M> + BayesianSurrogate<T, M> + Memory<T, M, M>,
+{
+    fn probe_acq(&self, surrogate: &S, x: &[T]) -> Option<T> {
+        let mean = surrogate.probe(x)?;
+        let sigma = surrogate.probe_variance(x)?.sqrt();
+        let min = *surrogate
+            .memory()
+            .min()
+            .expect("Obeservations must not be empty!")
+            .2;
+
+        let z = (min - mean - self.xi) / sigma;
+
+        let cdf =
+            T::from_f64(NORMAL.cdf(T::to_f64(&z).expect("Converting `T` to f64 must not fail.")))
+                .expect("Converting f64 to `T` must not fail.");
+        let pdf =
+            T::from_f64(NORMAL.pdf(T::to_f64(&z).expect("Converting `T` to f64 must not fail.")))
+                .expect("Converting f64 to `T` must not fail.");
+
+        Some(sigma * (z * cdf + pdf))
+    }
+}
+
+// impl AcqJacobian for EI<T>
