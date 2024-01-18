459b1d62896793bd35aefd2f528c26307a4062b8
Emile Visser <emile.visser@gmail.com>
2024-01-18 15:48:15 +0200

    Added temp bounds

--- src/bounds.rs
+++ src/bounds.rs
@@ -1,8 +1,21 @@
-use faer_core::ComplexField;
+
+use crate::dtype;
 
 pub trait Bounds<T>
 where
-    T: ComplexField<Unit = T>,
+    T: dtype,
 {
     fn inside(x: &[T]) -> bool;
+}
+
+
+pub struct ContinuousBounds {}
+
+impl<T> Bounds<T> for ContinuousBounds 
+where
+    T: dtype
+{
+    fn inside(_: &[T]) -> bool {
+        todo!()
+    }
 }
